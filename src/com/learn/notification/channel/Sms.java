85ff8beca2edba35f5a6f2c136fb1d580641868d
Chotika-SE62 <59428867+Chotika-SE62@users.noreply.github.com>
2021-08-09 16:27:53 +0700

    Initial commit

---
+++ src/com/learn/notification/channel/Sms.java
@@ -0,0 +1,11 @@
+package com.learn.notification.channel;
+
+import com.learn.notification.channel.interf.Notifiable;
+
+public class Sms implements Notifiable {
+	
+	public void send() {
+		System.out.println("Send Sms to Customer");
+	}
+	
+}
